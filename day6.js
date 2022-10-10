31a44d55f6edfa5c472b30daebca7d43861f9771
sethuthemonster <ananthasudhan20172gmail.com>
2022-10-10 17:33:34 +0530

    day6task

---
+++ day6.js
@@ -0,0 +1,33 @@
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i = 1; i < 11; i--) {
+ new_string += numsArr[i] 
+}
+console.log(new_string);
+
+
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i = 1; i < 11; i++) {
+ new_string += numsArr[i] 
+}
+console.log(new_string);
+
+var new_string = "";
+ 
+for (var i = 11; i > 0; i ) {
+ new_string += numsArr[i] + " "
+}
+console.log(new_string);
+
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (var i = 0; i <=10; i++) {
+ if(numsArr[i] %2 == 0 )
+ {
+ numsArr[i] = odd
+ }
+}
+console.log(numsArr);
